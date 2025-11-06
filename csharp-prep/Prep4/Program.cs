e1235dbec076bb7505a13a03b28c7a1da912ad94
claytyl <claytyl2000@gmail.com>
2025-11-05 17:37:38 -0700

    Homework catch up

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            int value;
+
+            if (!int.TryParse(input, out value))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                continue;
+            }
+
+            if (value == 0)
+                break;
+
+            numbers.Add(value);
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        int sum = 0;
+        foreach (int n in numbers)
+            sum += n;
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int n in numbers)
+            if (n > max) max = n;
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
---
+++ sandbox/SolarSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+class Program
+
+{
+    static void Main()
+    {
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 1234.343;
+
+        mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 1234.343;
+
+        venus.DisplayPlanetInformation();
+
+        SolarSystem solarSystem = new SolarSystem();
+        solar
+
+        
+    }
+}
