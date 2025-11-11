cbc6cbb6a7a3110e05f2bf5fa04ebae727b966c3
claytyl <claytyl2000@gmail.com>
2025-11-11 09:49:26 -0700

    Set radii

---
+++ sandbox/CircleProject/Circle.cs
@@ -0,0 +1,24 @@
+class Circle
+{
+    private double _radius;
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine("Invalid radius, must >= 0");
+            _radius = 0;
+        }
+        else
+        {
+            _radius = radius;
+        }
+
+    }
+
+    public double GetCircleArea()
+    {
+    return 3.1415 * _radius * _radius;
+    }
+}
+
--- sandbox/CircleProject/Program.cs
+++ sandbox/CircleProject/Program.cs
@@ -4,5 +4,10 @@
     static void Main()
     {
         Console.WriteLine("Oi como esta voce?");
+        Circle myCircle = new Circle();
+
+        myCircle.SetRadius(10);
+
+        Console.WriteLine(myCircle.GetCircleArea());
     }
 }
